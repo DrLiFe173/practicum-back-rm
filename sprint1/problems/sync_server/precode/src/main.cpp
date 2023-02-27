59ad5b98dcc692dee51d7ea484915533c938179c
DrLiFe173 <64245104+DrLiFe173@users.noreply.github.com>
2023-02-27 16:31:01 +0300

    S1 P2 Sync_server
    Sprint 1 Part 2 Sync_server code

--- sprint1/problems/sync_server/precode/src/main.cpp
+++ sprint1/problems/sync_server/precode/src/main.cpp
@@ -1,10 +1,11 @@
 #ifdef WIN32
 #include <sdkddkver.h>
 #endif
-// boost.beast будет использовать std::string_view вместо boost::string_view
+
 #define BOOST_BEAST_USE_STD_STRING_VIEW
 
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/write.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <iostream>
@@ -16,7 +17,114 @@ using tcp = net::ip::tcp;
 using namespace std::literals;
 namespace beast = boost::beast;
 namespace http = beast::http;
+using StringRequest = http::request<http::string_body>;
+using StringResponse = http::response<http::string_body>;
+
+struct ContentType {
+    ContentType() = delete;
+    constexpr static std::string_view TEXT_HTML = "text/html"sv;    
+};
+
+StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
+    bool keep_alive,
+    std::string_view content_type = ContentType::TEXT_HTML) {
+    StringResponse response(status, http_version);
+    response.set(http::field::content_type, content_type);
+    if (status == http::status::method_not_allowed) {
+        response.set(http::field::allow, "GET, HEAD"sv);
+    }
+    response.body() = body;
+    response.content_length(body.size());
+    response.keep_alive(keep_alive);
+    return response;
+}
+
+StringResponse HandleRequest(StringRequest&& req) {
+    const auto text_response = [&req](http::status status, std::string_view text) {
+        return MakeStringResponse(status, text, req.version(), req.keep_alive());
+    };
+    http::status response_status;
+    std::string response_message;
+    std::string target_text(req.target());
+
+    switch (req.method())
+    {
+    case http::verb::get:
+        response_message = "<strong>Hello, ";
+        response_message.append(target_text.erase(0, 1));
+        response_message.append("</strong>");
+        response_status = http::status::ok;
+        break;
+    case http::verb::head:
+        response_status = http::status::ok;        
+        break;
+    default:
+        response_status = http::status::method_not_allowed;
+        response_message = "Invalid method";
+    };
+    return text_response(response_status, response_message);
+}
+
+std::optional<StringRequest> ReadRequest(tcp::socket& socket, beast::flat_buffer& buffer) {
+    beast::error_code ec;
+    StringRequest req;
+
+    http::read(socket, buffer, req, ec);
+
+    if (ec == http::error::end_of_stream) {
+        return std::nullopt;
+    }
+
+    if (ec) {
+        throw std::runtime_error("Failed to read request: "s.append(ec.message()));
+    }
+
+    return req;
+}
+
+void DumpRequest(const StringRequest& req) {
+    std::cout << req.method_string() << ' ' << req.target() << std::endl;
+    for (const auto& header : req) {
+        std::cout << "  "sv << header.name_string() << ": "sv << header.value() << std::endl;
+    }
+}
+
+template <typename RequestHandler>
+void HandleConnection(tcp::socket& socket, RequestHandler&& handle_request) {
+    try {        
+        beast::flat_buffer buffer;
+
+        while (auto request = ReadRequest(socket, buffer)) {
+            DumpRequest(*request);
+            StringResponse response = handle_request(*std::move(request));
+            http::write(socket, response);
+            if (response.need_eof()) {
+                break;
+            }
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    beast::error_code ec;
+    socket.shutdown(tcp::socket::shutdown_send, ec);
+}
 
 int main() {
-    // Выведите строчку "Server has started...", когда сервер будет готов принимать подключения
+    net::io_context ioc;
+    const auto address = net::ip::make_address("0.0.0.0");
+    constexpr unsigned short port = 8080;
+    tcp::acceptor acceptor(ioc, { address, port });
+    std::cout << "Server has started..."sv << std::endl;
+
+    while (true) {
+        tcp::socket socket(ioc);
+        acceptor.accept(socket);
+        std::thread t(
+            [](tcp::socket socket) {                
+                HandleConnection(socket, HandleRequest);
+            },
+            std::move(socket));
+        t.detach();
+    }
 }
