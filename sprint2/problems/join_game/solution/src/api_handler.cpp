491b8b3f9171c7e900faa42f863f3a0d63b70d56
DrLiFe173 <64245104+DrLiFe173@users.noreply.github.com>
2023-03-23 19:25:17 +0300

    S2 P1 join_game fix2
    Sprint 2 Part 1 join_game fix2

--- sprint2/problems/join_game/solution/src/api_handler.cpp
+++ sprint2/problems/join_game/solution/src/api_handler.cpp
@@ -82,8 +82,10 @@ namespace http_handler {
         }
     }
 
-    StringResponse ApiRequestHandler::ProceedJoinGameRequest(json::value& jsonValue)
-    {       
+    StringResponse ApiRequestHandler::ProceedJoinGameRequest(std::string& body)
+    {
+        try {
+            auto jsonValue = json::parse(body);
             std::string mapId = jsonValue.as_object().at("mapId"s).as_string().data();
             std::string userName = jsonValue.as_object().at("userName"s).as_string().data();
             model::Map::Id id{ mapId };
@@ -118,6 +120,11 @@ namespace http_handler {
             else {
                 return Response::MakeJSON(http::status::not_found, ErrorCode::MAP_NOT_FOUND, ErrorMessage::MAP_NOT_FOUND);
             }
+        }
+        catch (...)
+        {
+            return Response::MakeBadRequestInvalidArgument(ErrorMessage::PARSE_ERROR);
+        }
     }
 
     StringResponse ApiRequestHandler::ProceedPlayerListRequest(std::string_view& tokenValue)
